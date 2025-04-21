6e91f21001c2083dada641877b043bfb9996f8c8
seominseok00 <seominseok4834@gmail.com>
2025-04-21 22:04:32 +0900

    SAC added

---
+++ sac/model.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.distributions.normal import Normal
+
+class MLPActor(nn.Module):
+    LOG_STD_MAX = 2
+    LOG_STD_MIN = -20
+
+    def __init__(self, obs_dim, hid_dim, act_dim, activation=F.tanh, act_limit=1):
+        super(MLPActor, self).__init__()
+        self.fc1 = nn.Linear(obs_dim, hid_dim)
+        self.fc2 = nn.Linear(hid_dim, hid_dim)
+        self.mu_layer = nn.Linear(hid_dim, act_dim)
+        self.log_std_layer = nn.Linear(hid_dim, act_dim)
+        self.activation = activation
+        self.act_limit = act_limit
+
+    def forward(self, obs, deterministic=False, with_logprob=True):
+        x = self.activation(self.fc1(obs))
+        net_out = self.activation(self.fc2(x))
+        mu = self.mu_layer(net_out)
+        log_std = self.log_std_layer(net_out)
+        log_std = torch.clamp(log_std, self.LOG_STD_MIN, self.LOG_STD_MAX)
+        std = torch.exp(log_std)
+
+        pi_distribution = Normal(mu, std)
+        if deterministic:
+            pi_action = mu
+        else:
+            pi_action = pi_distribution.rsample()  # Draw a sample with reparameterization for gradient backpropagation
+
+        if with_logprob:
+            # Compute logprob from Gaussian, and then apply correction for Tanh squashing.
+            # NOTE: The correction formula is a little bit magic. To get an understanding 
+            # of where it comes from, check out the original SAC paper (arXiv 1801.01290) 
+            # and look in appendix C. This is a more numerically-stable equivalent to Eq 21.
+            # Try deriving it yourself as a (very difficult) exercise. :)
+            logp_pi = pi_distribution.log_prob(pi_action).sum(axis=-1)
+            logp_pi -= (2*(np.log(2) - pi_action - F.softplus(-2*pi_action))).sum(axis=1)
+        else:
+            logp_pi = None
+
+        pi_action = torch.tanh(pi_action)
+        pi_action = self.act_limit * pi_action
+
+        return pi_action, logp_pi
+    
+
+class MLPQFunction(nn.Module):
+    def __init__(self, obs_dim, act_dim, hid_dim, activation=F.tanh):
+        super(MLPQFunction, self).__init__()
+        self.fc1 = nn.Linear(obs_dim + act_dim, hid_dim)
+        self.fc2 = nn.Linear(hid_dim, hid_dim)
+        self.fc3 = nn.Linear(hid_dim, 1)
+        self.activation = activation
+
+    def forward(self, obs, act):
+        x = self.activation(self.fc1(torch.cat([obs, act], dim=-1)))
+        x = self.activation(self.fc2(x))
+        q = self.fc3(x)
+        return torch.squeeze(q, -1)
+    
+    
+class MLPActorCritic(nn.Module):
+    init_alpha = 0.2
+
+    def __init__(self, obs_space, act_space, hid_dim=64, activation=F.tanh):
+        super(MLPActorCritic, self).__init__()
+
+        obs_dim = obs_space.shape[0]
+        act_dim = act_space.shape[0]
+        act_limit = act_space.high[0]
+
+        self.pi = MLPActor(obs_dim, hid_dim, act_dim, activation, act_limit)
+
+        self.q1 = MLPQFunction(obs_dim, act_dim, hid_dim, activation)
+        self.q2 = MLPQFunction(obs_dim, act_dim, hid_dim, activation)
+
+        self.qc1 = MLPQFunction(obs_dim, act_dim, hid_dim, activation)
+        self.qc2 = MLPQFunction(obs_dim, act_dim, hid_dim, activation)
+
+        self.log_alpha = torch.tensor(np.log(self.init_alpha), requires_grad=True)
+        
+    def act(self, obs, deterministic=False):
+        with torch.no_grad():
+            a, _ = self.pi(obs, deterministic, False)
+            return a.numpy()
---
+++ sac/sac.py
@@ -0,0 +1,245 @@
+import time
+import itertools
+from copy import deepcopy
+from collections import defaultdict, deque
+
+import numpy as np
+import pandas as pd
+
+import torch
+from torch.optim import Adam
+
+import safety_gymnasium
+
+from model import MLPActorCritic
+from buffer import Buffer
+
+def sac(env_fn, actor_critic=MLPActorCritic, ac_kwargs=dict(), seed=0,
+        epochs=100, steps_per_epoch=4000, replay_size=int(1e6), batch_size=100,
+        gamma=0.99, polyak=0.995, pi_lr=1e-3, q_lr=1e-3, alpha_lr=1e-3, start_steps=10000, 
+        update_after=1000, update_interval=50, update_iters=50, max_ep_len=1000):
+    
+    epoch_logger = []
+
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    env = env_fn()
+    obs_dim = env.observation_space.shape[0]
+    act_dim = env.action_space.shape[0]
+
+    ac = actor_critic(env.observation_space, env.action_space, **ac_kwargs)
+    ac_targ = deepcopy(ac)
+
+    buf = Buffer(obs_dim, act_dim, replay_size)
+
+    # Freeze target networks with respect to optimizers (only update via polyak averaging)
+    for p in ac_targ.parameters():
+        p.requires_grad = False
+
+    q_params = itertools.chain(ac.q1.parameters(), ac.q2.parameters())
+
+    pi_optimizer = Adam(ac.pi.parameters(), lr=pi_lr)
+    q_optimizer = Adam(q_params, lr=q_lr)
+    alpha_optimizer = Adam([ac.log_alpha], lr=alpha_lr)
+
+    target_entropy = -float(act_dim)
+
+    #=====================================================================#
+    #  Loss function for update policy                                    #
+    #=====================================================================#
+
+
+    def compute_loss_pi(data):
+        o = data['obs']
+
+        a, logp_a = ac.pi(o)
+
+        q1 = ac.q1(o, a)
+        q2 = ac.q2(o, a)
+        q = torch.min(q1, q2)
+
+        # Entropy regularized policy loss
+        loss_pi = (ac.log_alpha.exp() * logp_a - q).mean()
+
+        loss_alpha = -(ac.log_alpha.exp() * (logp_a + target_entropy).detach()).mean()
+
+        return loss_pi, loss_alpha
+    
+    #=====================================================================#
+    #  Loss function for update q function                                #
+    #=====================================================================#
+
+    def compute_loss_q(data):
+        o, a, r, o2, d = data['obs'], data['act'], data['rew'], data['obs2'], data['done']
+
+        q1 = ac.q1(o, a)
+        q2 = ac.q2(o, a)
+
+        # Bellman backup for Q functions
+        with torch.no_grad():
+            # Target actions come from current policy
+            a2, logp_a2 = ac.pi(o2)
+
+            # Target Q-values
+            q1_target = ac_targ.q1(o2, a2)
+            q2_target = ac_targ.q2(o2, a2)
+            q_target = torch.min(q1_target, q2_target)
+
+            backup_q = r + gamma * (1 - d) * (q_target - ac.log_alpha.exp() * logp_a2)
+
+        # MSE loss against Bellman backup
+        loss_q1 = ((q1 - backup_q) ** 2).mean()
+        loss_q2 = ((q2 - backup_q) ** 2).mean()
+        loss_q = loss_q1 + loss_q2
+
+        return loss_q
+
+    def update(data):
+        train_logger = {
+            'loss_pi': [],
+            'loss_q': [],
+            'loss_alpha': []
+        }
+
+        #=====================================================================#
+        #  Update q function                                                  #
+        #=====================================================================#
+
+        loss_q = compute_loss_q(data)
+
+        q_optimizer.zero_grad()
+        loss_q.backward()
+        q_optimizer.step()
+
+        train_logger['loss_q'].append(loss_q.item())
+
+        # Freeze Q-networks for computational efficiency
+        for p in q_params:
+            p.requires_grad = False
+
+        #=====================================================================#
+        #  Update policy function and alpha                                   #
+        #=====================================================================#
+
+        loss_pi, loss_alpha = compute_loss_pi(data)
+
+        pi_optimizer.zero_grad()
+        loss_pi.backward()
+        pi_optimizer.step()
+
+        alpha_optimizer.zero_grad()
+        loss_alpha.backward()
+        alpha_optimizer.step()
+
+        train_logger['loss_pi'].append(loss_pi.item())
+        train_logger['loss_alpha'].append(loss_alpha.item())
+
+        # Unfreeze Q-networks
+        for p in q_params:
+            p.requires_grad = True
+
+        #=====================================================================#
+        #  Update target networks                                             #
+        #=====================================================================#
+        with torch.no_grad():
+            for p, p_targ in zip(ac.parameters(), ac_targ.parameters()):
+                p_targ.data.mul_(polyak)
+                p_targ.data.add_((1 - polyak) * p.data)
+
+        return train_logger
+
+    def get_action(o, deterministic=False):
+        return ac.act(torch.as_tensor(o, dtype=torch.float32), deterministic)
+
+    
+    #=========================================================================#
+    #  Run main environment interaction loop                                  #
+    #=========================================================================#
+    
+    start_time = time.time()
+
+    total_steps = 0
+
+    episode_per_epoch = steps_per_epoch // max_ep_len
+    rollout_logger = {
+        'EpRet': deque(maxlen=episode_per_epoch),
+        'EpCost': deque(maxlen=episode_per_epoch),
+        'EpLen': deque(maxlen=episode_per_epoch),
+    }
+
+    update_logger = defaultdict(list)
+
+    o, _ = env.reset()
+    ep_ret, ep_cret, ep_len = 0, 0, 0
+
+    for epoch in range(epochs):
+        for t in range(steps_per_epoch):
+            total_steps += 1
+
+            if total_steps > start_steps:
+                a = get_action(o)
+            else:
+                a = env.action_space.sample()
+
+            next_o, r, c, d, truncated, info = env.step(a)
+
+            ep_ret += r
+            ep_cret += c
+            ep_len += 1
+
+            buf.store(o, a, r, c, next_o, d)
+
+            o = next_o
+
+            timeout = ep_len == max_ep_len
+            terminal = d or timeout
+            epoch_ended = t == steps_per_epoch - 1
+
+            if terminal or epoch_ended:
+                rollout_logger['EpRet'].append(ep_ret)
+                rollout_logger['EpCost'].append(ep_cret)
+                rollout_logger['EpLen'].append(ep_len)
+
+                o, _ = env.reset()
+                ep_ret, ep_cret, ep_len = 0, 0, 0
+
+            #=====================================================================#
+            #  Run RL update                                                      #
+            #=====================================================================#
+            
+            if total_steps >= update_after and total_steps % update_interval == 0:
+                for j in range(update_iters):
+                    batch = buf.sample_batch(batch_size)
+                    train_logger = update(batch)
+
+                    for k, v in train_logger.items():
+                        update_logger[k] += v
+
+        #=====================================================================#
+        #  Log performance and stats                                          #
+        #=====================================================================#
+
+        epoch_logger.append({
+            'epoch': epoch,
+            'EpRet': np.mean(rollout_logger['EpRet']),
+            'EpCost': np.mean(rollout_logger['EpCost']),
+            'EpLen': np.mean(rollout_logger['EpLen']),
+            'loss_pi': np.mean(update_logger['loss_pi']),
+            'loss_q': np.mean(update_logger['loss_q']),
+            'loss_alpha': np.mean(update_logger['loss_alpha'])
+        })
+
+        print('Epoch: {} avg return: {}, avg cost: {}'.format(epoch, np.mean(rollout_logger['EpRet']), np.mean(rollout_logger['EpCost'])))
+        print('Loss pi: {}, Loss q: {}, Loss alpha: {}\n'.format(np.mean(update_logger['loss_pi']), np.mean(update_logger['loss_q']), np.mean(update_logger['loss_alpha'])))
+
+        update_logger.clear()
+        
+    end_time = time.time()
+    print('Training time: {}h {}m {}s'.format(int((end_time - start_time) // 3600), int((end_time - start_time) % 3600 // 60), int((end_time - start_time) % 60)))
+    
+    update_logger_df = pd.DataFrame(update_logger)
+    update_logger_df.to_csv('update_logger.csv', index=False)
+
+if __name__ == '__main__':
+    sac(lambda: safety_gymnasium.make('SafetyPointButton1-v0'))
