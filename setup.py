5b12fb606db6a70b0f12859c0e3bd4ba5b0b1499
seominseok00 <seominseok4834@gmail.com>
2025-09-10 16:15:47 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="safe_rl",
+    version="0.1.0",
+    author="Minseok Seo",
+    author_email="seominseok4834@gmail.com",
+    url="https://github.com/seominseok00/SafeRL",  # 깃헙 주소
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.6",
+)
